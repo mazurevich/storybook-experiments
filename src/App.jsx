c8d7751c943fe40998188f8aea67360f1990f323
Andrey_Mazurevich <mazyrevich@gmail.com>
2023-02-20 21:56:43 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+console.log("i ii ii iHello world");
+function App() {
+  return (
+    <div>
+      <h1 className="border-green-100  bg-gray-800 p-4 pt-2 text-vuejs md:p-3">
+        Hello world
+      </h1>
+      <h1 className="mt-2 mb-4 bg-gray-800 p-3 text-green-400  md:bg-green-400">
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
